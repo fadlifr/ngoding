36452593a0f98ebbb8f5c3eb93ef36b12854d1f7
ffrwebdev <ffr.learning@gmail.com>
2023-10-28 23:12:02 +0700

    add feat/hero and feat/services

--- script.js
+++ script.js
@@ -6,8 +6,48 @@ hamburgerMenu.addEventListener('click', () => {
   navMenu.classList.toggle('active')
 })
 
-document.querySelectorAll('.nav-link').forEach(link => link.addEventListener('click', (event) => {
-  event.preventDefault()
+document.querySelectorAll('.nav-link').forEach(link => link.addEventListener('click', () => {
+  // event.preventDefault()
   hamburgerMenu.classList.remove('active')
   navMenu.classList.remove('active')
 }))
+
+// window.addEventListener('resize', () => {
+//   if (window.innerWidth > 756) {
+//     hamburgerMenu.classList.remove('active')
+//     navMenu.classList.remove('active')
+//   }
+// })
+
+// Navbar hidden when scroll-down, show when scroll-up
+const header = document.querySelector('.header-container');
+let lastScrollPosition = window.scrollY
+let scrollingUp = false;
+
+window.addEventListener('scroll', () => {
+    const currentScrollPosition = window.scrollY
+
+    if (currentScrollPosition < lastScrollPosition) {
+        // Scrolling up, show the navbar
+        if (!scrollingUp) {
+            header.style.transform = 'translateY(0)';
+            scrollingUp = true;
+        }
+    } else {
+        // Scrolling down, hide the navbar
+        header.style.transform = 'translateY(-100%)';
+        scrollingUp = false;
+    }
+
+    lastScrollPosition = currentScrollPosition;
+});
+
+// to top icon
+const toTop = document.querySelector('.to-top')
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        toTop.classList.add('active')
+    } else {
+        toTop.classList.remove('active')
+    }
+})
